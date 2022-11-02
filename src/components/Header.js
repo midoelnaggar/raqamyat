7019f60ba4403738f1f30cacf8bfece1b509ea20
midoelnaggar <mido.elnaggar@gmail.com>
2022-11-02 18:00:31 +0200

    EO011122

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,21 @@ function Header() {
   return (
     <div className="header">
       <div className="nav-list">
-        <ul>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/media">Media</a>
-          </li>
-        </ul>
+        <a href="/home">Home</a>
+        <p>{" ‧ "}</p>
+        <a href="/about">About</a>
+        <p>{" ‧ "}</p>
+        <a href="/services">Services</a>
+        <p>{" ‧ "}</p>
+        <a href="/blog">Blog</a>
+        <p>{" ‧ "}</p>
+        <a href="/media">Media</a>
       </div>
       <div className="logo">
-        <img src={logo} />
+        <img className="logo-img" src={logo} />
       </div>
-      <div className="contact-us-container">
-        <img src={logo} />
+      <div className="contact-us">
+        <button className="btn">Contact Us</button>
       </div>
     </div>
   );
